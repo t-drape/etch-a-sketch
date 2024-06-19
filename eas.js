046b345d0d22d08675759c51552b46c778ec4f94
t-drape <thebatreview1@gmail.com>
2024-06-18 20:31:19 -0700

    Add color change upon hover

--- eas.js
+++ eas.js
@@ -1,12 +1,24 @@
-container = document.querySelector(".container");
+const container = document.querySelector(".container");
 
-let width = 960 / 4
+const button = document.querySelector("button");
 
-for (let i = 0; i < 4**2; i++) {
-    grid_element = document.createElement("div");
-    grid_element.style.backgroundColor = "white";
-    grid_element.style.width = width;
-    grid_element.style.height = width;
-    grid_element.style.flex = "auto";
-    container.appendChild(grid_element);
-}
+button.addEventListener("click", () => {
+    console.log(container)
+    let number = prompt("How many squares?", 16)
+    number = +number;
+    let width = 960 / number
+
+    for (let i = 0; i < number**2; i++) {
+        grid_element = document.createElement("div");
+        grid_element.style.backgroundColor = "white";
+        grid_element.style.width = width;
+        grid_element.style.height = width;
+        grid_element.style.flex = "auto";
+        container.appendChild(grid_element);
+    }
+    container.onmouseover = (e) => {
+        e.target.style.backgroundColor = "black";
+    }
+
+});
+
